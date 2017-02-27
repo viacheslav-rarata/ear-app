bb71562b234525cd120942ef65b4c2b59ec7cdf1
Vyacheslav Rarata <cayneir@gmail.com>
2017-02-27 18:21:04 +0200

    - added properties to pom and application.properties - load properties in LoginController

--- login/src/main/java/org/app/login/LoginController.java
+++ login/src/main/java/org/app/login/LoginController.java
@@ -5,11 +5,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class LoginController extends HttpServlet {
 
-    private final String moduleApp1 = "http://localhost:7001/my-app1/login/app1";
-    private final String moduleApp2 = "http://localhost:7001/my-app2/app2";
+    private String moduleApp1;
+    private String moduleApp2;
+
+    public LoginController() {
+        Properties properties = loadResources();
+        initModules(properties);
+    }
+
+    private Properties loadResources() {
+        Properties properties = new Properties();
+        try {
+            InputStream fileInputStream = getClass().getClassLoader().getResourceAsStream("application.properties");
+            properties.load(fileInputStream);
+        } catch (Exception e ) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+        return properties;
+    }
+
+    private void initModules(Properties properties) {
+        moduleApp1 = "http://localhost:7001" + properties.getProperty("app1.root") + "/login/app1";
+        moduleApp2 = "http://localhost:7001 "+ properties.getProperty("app2.root") + "/app2";
+    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         final String uri = request.getRequestURI();
@@ -29,5 +53,6 @@ public class LoginController extends HttpServlet {
         response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
         response.setHeader("Location", url);
     }
+
 }
 
